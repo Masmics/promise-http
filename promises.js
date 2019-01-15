8b0cf87b58ec2955f197b517b696b0769c893513
Masmics <cherlocker3113@gmail.com>
2019-01-15 10:37:33 -0800

    promises ex IP

--- promises.js
+++ promises.js
@@ -1,7 +1,6 @@
 const fsPromises = require('fs').promises;
 const readIt = ('./.gitignore');
 
-
 // pending state - waiting for promise to finish
 // fulfilled - finished and resolved
 // rejected - finished and rejected
@@ -10,7 +9,17 @@ fsPromises.readFile(readIt, { encoding: 'utf8' })
   .then(data => console.log(data))
   .catch(err => console.error(err));
 
-fsPromises.writeFile(readIt, 'sampleText')
+fsPromises.writeFile('test.txt', 'sampleText')
   .then(() => console.log('done!'))
   .catch(err => console.error(err));
+
+//function promisesCopy(`readIt`, 'test.txt', 'sampleText') {
+fsPromises.readFile(readIt, { encoding: 'utf8' })
+  .then(data => fsPromises.writeFile('test.txt', data))
+  .catch(err => console.error(err));
+
+fsPromises.writeFile('test.txt', 'sampleText')
+  .then(() => console.log('write step complete!'))
+  .catch(err => console.error(err));
+//}
 
