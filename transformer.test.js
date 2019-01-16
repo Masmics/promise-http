a8df998810cdb08b534e08e1f1e7e5821611b289
Masmics <cherlocker3113@gmail.com>
2019-01-15 19:05:28 -0800

    work at home

--- transformer.test.js
+++ transformer.test.js
@@ -3,7 +3,14 @@ const fsPromises = require('fs').promises;
 
 describe('transformer', () => {
   it('transforms some file', () => {
-    return transformer('./transform.txt')
+    return transformer('./transform.txt') ()
       .then(transTxt => expect(transTxt).toEqual('EREH I'),
   )}
-)};
+// )};
+
+  });
+
+  it('removes capital letters', () => {
+    expect(removeCapitals('Hi There')).toEqual('i here');
+  });
+});
