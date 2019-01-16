653e8cca64c5b750378fb75def5b7ea267a1cee4
Masmics <cherlocker3113@gmail.com>
2019-01-15 20:15:30 -0800

    transformer and test debugging

--- transformer.js
+++ transformer.js
@@ -1,18 +1,5 @@
 const fsPromises = require('fs').promises;
 
-const removeCapitals = str => {
-  return str
-    .split('')
-    .filter(letter => {
-      return letter === letter.toLowerCase();
-    })
-    .join('');
-
-};
-const makeAllLettersCapital = str => str.toUpperCase();
-const reverse = str => str.split(''.reverse().join(''));
-const trim = str => str.trim();
-
 const transformer = src => {
   fsPromises.readFile(src, { encoding: 'utf8' })
     .then(data => removeCapitals(data))
@@ -24,9 +11,45 @@ const transformer = src => {
   //then make all caps
   //then reverse the string
 };
+const removeCapitals = str => {
+  return str
+    .split('')
+    .filter(letter => {
+      return letter === letter.toLowerCase();
+    })
+    .join('');
+
+};
+const makeAllLettersCapital = str => {
+  fsPromises.readFile(str, { encoding: 'utf8' });
+  return str.toUpperCase();
+};
+const reverse = str => {
+  fsPromises.readFile(str, { encoding: 'utf8' });
+  return str.split(reverse().join(''));
+};
+const trim = str => {
+  fsPromises.readFile(str, { encoding: 'utf8' });
+  return str.trim();
+};
+
+// const transformer = src => {
+//   fsPromises.readFile(src, { encoding: 'utf8' })
+//     .then(data => removeCapitals(data))
+//     .then(data => makeAllLettersCapital(data))
+//     .then(data => reverse(data))
+//     .then(data => trim(data));
+    
+//   //then remove caps
+//   //then make all caps
+//   //then reverse the string
+// };
 
 module.exports = {
   transformer,
-  removeCapitals
+  removeCapitals,
+  makeAllLettersCapital,
+  reverse,
+  trim
 };
 
--- transformer.test.js
+++ transformer.test.js
@@ -1,16 +1,18 @@
-// const { transformer } = require('./transformer');
-// const fsPromises = require('fs').promises;
+const transformer = require('./transformer');
+//const fsPromises = require('fs').promises;
 
-// describe('transformer', () => {
-//   it('transforms some file', () => {
-//     return transformer('./transform.txt') ()
-//       .then(transTxt => expect(transTxt).toEqual('EREH I'),
-//   )}
-// // )};
-
-//   });
-
-//   it('removes capital letters', () => {
-//     expect(removeCapitals('Hi There')).toEqual('i here');
-//   });
-// });
+describe('transformer', () => {
+  it('removes capital letters', () => {
+    expect(transformer.removeCapitals('Hi There')).toEqual('i here');
+  });
+  it('capitalizes all letters', () => {
+    expect(transformer.makeAllLettersCapital('i here')).toEqual('I HERE');
+  });
+  it('reverses the string', () => {
+    expect(transformer.reverse('I HERE')).toEqual('EREH I');
+  });
+  it('trims spaces from the string', () => {
+    expect(transformer.trim('EREH I')).toEqual('EREHI');
+  });
+  //expect(transformer('Hi There')).toEqual('EREHI');
+});
